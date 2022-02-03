ce2f5b66b8df89d8d1576b60d1a15eff3d3f9fd4
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2022-02-03 13:46:02 +0000

    added code for switching wallpaper based on datetime

---
+++ background.py
@@ -0,0 +1,24 @@
+from os import system as s
+from datetime import datetime
+
+sunrise = '06'
+noon = '12'
+sunset = '17'
+night = '20'
+
+time_of_day = datetime.now()
+real_time = time_of_day.strftime("%H")
+
+def switch_background(path):
+ 	s('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "{}"\''.format(path))
+    
+
+
+if real_time > sunrise and real_time < noon:
+    switch_background("/Users/stavros/Documents/GitHub/desktop_bg/Desktop_background/dawn.jpg")
+elif real_time <= sunrise:
+    switch_background("/Users/stavros/Documents/GitHub/desktop_bg/Desktop_background/night.jpg")
+elif real_time >= noon and real_time < sunset:
+    switch_background("/Users/stavros/Documents/GitHub/desktop_bg/Desktop_background/noon.jpg")
+elif real_time >= sunset and real_time < night:
+    switch_background("/Users/stavros/Documents/GitHub/desktop_bg/Desktop_background/sunset.jpg")
