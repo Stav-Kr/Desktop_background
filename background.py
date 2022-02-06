3219191052118da21d4d8e7a51ab9565e8fb1971
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2022-02-06 04:20:02 +0000

    added s_o_t function

--- background.py
+++ background.py
@@ -13,14 +13,18 @@ def switch_background(path):
  	s('osascript -e \'tell application "Finder" to set picture of every desktop to POSIX file "{}"\''.format(path))
     
 
-
-if real_time > sunrise and real_time < noon:
-    switch_background("dawn.jpg")
-elif real_time <= sunrise:
-    switch_background("night.jpg")
-elif real_time >= noon and real_time < sunset:
-    switch_background("noon.jpg")
-elif real_time >= sunset and real_time < night:
-    switch_background("sunset.jpg")
-elif real_time >= night:
-    switch_background("night.jpg")
+def switch_on_time():
+    if real_time > sunrise and real_time < noon:
+        switch_background("dawn.jpg")
+    elif real_time <= sunrise:
+        switch_background("night.jpg")
+    elif real_time >= noon and real_time < sunset:
+        switch_background("noon.jpg")
+    elif real_time >= sunset and real_time < night:
+        switch_background("sunset.jpg")
+    elif real_time >= night:
+        switch_background("night.jpg")
+ 
+ 
+if __name__ == "__main__":
+    switch_on_time()
